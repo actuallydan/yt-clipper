1bea7044f32cad8eb9e5e322f387f82ed6ab6592
Dan Kral <dankral01@gmail.com>
2023-08-21 11:12:19 -0400

    proof of concept

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = 3030;
 const videoDirectory = "clipped_videos"; // Specify the directory to save clipped videos
 
-app.get("/stream", async (req: Request, res: Response) => {
+app.get("/clip", async (req: Request, res: Response) => {
   const youtubeLink = req.query.link as string;
   const start = req.query.start as string;
   const end = req.query.end as string;
@@ -17,79 +17,65 @@ app.get("/stream", async (req: Request, res: Response) => {
   }
 
   try {
+    if (!fs.existsSync(videoDirectory)) {
+      fs.mkdirSync(videoDirectory);
+    }
 
     let videoTitle = (await youtubedl(youtubeLink, {
       getFilename: true,
     })) as unknown as string;
 
-    const clippedVideoFilename = `${videoTitle}-${start}-${end}.mp4`;
+    const clippedVideoFilename = `${
+      Date.now().toString() + "-" + Math.random().toString().replace(".", "")
+    }.mp4`;
     const clippedVideoPath = `${videoDirectory}/${clippedVideoFilename}`;
 
-    if (!fs.existsSync(clippedVideoPath)) {
-      await youtubedl(youtubeLink);
-      console.log("downloaded the file");
-      const newFullPath = `${videoDirectory}/${encodeURI(videoTitle)}`;
-      console.log("about to move file");
-      fs.renameSync(videoTitle, newFullPath);
-      console.log("moved file");
-      videoTitle = newFullPath;
+    // if (!fs.existsSync(clippedVideoPath)) {
+    await youtubedl(youtubeLink);
 
-      console.log("about to clip");
-      const ffmpegProcess = spawn("ffmpeg", [
-        "-ss",
-        start,
-        "-i",
-        videoTitle,
-        "-to",
-        end,
-        "-c:v",
-        "libx264",
-        "-preset",
-        "ultrafast",
-        "-tune",
-        "zerolatency",
-        "-movflags",
-        "frag_keyframe+empty_moov",
-        "-f",
-        "mp4",
-        clippedVideoPath,
-      ]);
+    const newFullPath = `${videoDirectory}/${encodeURI(videoTitle)}`;
 
-      ffmpegProcess.stderr.on("data", (data) => {
-        console.error(`FFmpeg error: ${data}`);
-      });
-      ffmpegProcess.on("close", (code) => {
-        if (code !== 0) {
-          console.error(`FFmpeg process exited with code ${code}`);
-        }
+    fs.renameSync(videoTitle, newFullPath);
 
-        // const stream = fs.createReadStream(clippedVideoPath);
+    videoTitle = newFullPath;
 
-        // res.setHeader('Content-Type', 'video/mp4');
-        // res.setHeader('Accept-Ranges', 'bytes');
-        // stream.pipe(res);
+    const ffmpegProcess = spawn("ffmpeg", [
+      "-ss",
+      start,
+      "-i",
+      newFullPath,
+      "-to",
+      end,
+      "-c:v",
+      "libx264",
+      "-preset",
+      "ultrafast",
+      "-tune",
+      "zerolatency",
+      "-movflags",
+      "frag_keyframe+empty_moov",
+      "-f",
+      "mp4",
+      clippedVideoPath,
+    ]);
 
-        res.setHeader("Content-Type", "text/html");
+    ffmpegProcess.stderr.on("data", (data) => {
+      // for some reason ffmpeg's output is logged as stderr...
+      console.error(`FFmpeg error: ${data}`);
+    });
 
-        res.send(`
+    ffmpegProcess.on("close", (code) => {
+      if (code !== 0) {
+        console.error(`FFmpeg process exited with code ${code}`);
+      }
+
+      res.setHeader("Content-Type", "text/html");
+
+      res.send(`
         <video src="${clippedVideoPath}" autoplay="true">
         </video>
         `);
-      });
-    }
-
-    const stream = fs.createReadStream(clippedVideoPath);
-
-    // res.setHeader('Content-Type', 'video/mp4');
-    // res.setHeader('Accept-Ranges', 'bytes');
-    // stream.pipe(res);
-
-    res.setHeader("Content-Type", "text/html");
-
-    res.send(`
-        <video src="${clippedVideoPath}" autoplay="true">
-        </video>
-        `);
+    });
   } catch (error) {
     console.error(`Error: ${error}`);
     return res.status(500).send("An error occurred");
@@ -98,4 +84,16 @@ app.get("/stream", async (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+});
+
+app.get("/clipped_videos/:path", (req, res) => {
+  console.log(req.params);
+
+  const file = `clipped_videos/${req.params.path}`;
+  const length = fs.statSync(file);
+  const stream = fs.createReadStream(file);
+  res.setHeader("Content-Type", "video/mp4");
+  res.setHeader("Accept-Ranges", "bytes");
+  res.setHeader("Content-Length", length.size);
+  stream.pipe(res);
 });
